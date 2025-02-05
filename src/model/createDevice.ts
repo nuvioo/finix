9d092b431675442d5b4597dd44673f4ee31b3f55
cuongZDN <cuong@zodinet.com>
2025-02-05 16:25:46 +0700

    chore: update create device add serialNumber

--- src/model/createDevice.ts
+++ src/model/createDevice.ts
@@ -27,6 +27,10 @@ export class CreateDevice {
     * Key value pair for annotating custom meta data (e.g. order numbers).
     */
     'tags'?: { [key: string]: string; } | null;
+    /**
+    * The serial number of the `Device`.
+    */
+    'serialNumber': string;
 
     static discriminator: string | undefined = undefined;
 
@@ -55,6 +59,11 @@ export class CreateDevice {
             "name": "tags",
             "baseName": "tags",
             "type": "{ [key: string]: string; }"
+        },
+        {
+            "name": "serialNumber",
+            "baseName": "serial_number",
+            "type": "string"
         }    ];
 
     static getAttributeTypeMap() {
